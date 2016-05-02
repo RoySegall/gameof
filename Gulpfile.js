37156b064b28f106164ae92ca18d0562b15538ec
RoySegall <roy.segall@gizra.com>
2016-05-02 21:40:08 +0300

    Adding boileplate

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,10 +1,22 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var gutil = require('gulp-util');
+require('colors');
 
 gulp.task('styles', function() {
   gulp.src('front/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./front/css/'))
+});
+
+gulp.task('migration', function() {
+});
+
+gulp.task('reinstall-db', function() {
+});
+
+gulp.task('add-question', function() {
+
 });
 
 //Watch task
