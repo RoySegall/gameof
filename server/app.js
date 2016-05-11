cb61d33ac97ab81381d0d7751769aef89d615a42
RoySegall <roy.segall@gizra.com>
2016-05-11 14:37:35 +0300

    Adding plug.

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 var gameOf = require('./modules/modules');
+
 // Db stuff here.
 gameOf.yml.setYmlPath(__dirname + '/config/config.yml');
+gameOf.plug.setPluginsPath(__dirname + '/modules/plugins');
 
 // Run over all the plugins and get the information.
-gameOf.plug.getListPlugins();
+gameOf.plug.getPlugins();
--- server/modules/modules/plug.js
+++ server/modules/modules/plug.js
@@ -1,34 +1,17 @@
-var fs = require('fs');
-
-/**
- * Get all the plugins.
- *
- * @param modules
- *   The main module object.
- */
-var getListPlugins = function(modules) {
-
-  // Get all the modules except for this module.
-
-  // Get their location.
-
-  // Iterate over files and et the info from the annotation.
-
-  // construct an express object.
-
-};
-
-/**
- * Get the information on the plugin.
- *
- * @param file
- *   The file path.
- */
-var getPluginInfo = function(file) {
-  // return an object wih the path info and the methods.
-};
+var path = require('../../node_modules/path'), plugger = require('../../node_modules/plug').create();
 
 module.exports = {
-  getListPlugins: getListPlugins,
-  getPluginInfo: getPluginInfo
+
+  setPluginsPath: function(path) {
+    this.plguinsPath = path;
+  },
+
+  getPluginsPath: function() {
+    return this.plguinsPath;
+  },
+
+  getPlugins: function() {
+    plugger.find(path.resolve(this.getPluginsPath()));
+  }
+  
 };
