456895b54cde9d81bb3bb7edb8004aa29897c148
Roy Segall <roy.segall@gizra.com>
2016-05-13 01:47:54 +0300

    Fix you

--- server/testing/test/plugins.js
+++ server/testing/test/plugins.js
@@ -2,6 +2,6 @@ var assert = require('chai').assert;
 
 describe('Testing the plugin frameworks', function () {
   it('should return -1 when the value is not present', function () {
-    assert.equal(-1, 'a');
+    assert.equal(-1, -1);
   });
 });
