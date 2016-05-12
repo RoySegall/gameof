891bae31ed452ad21155d73576861141717cc64c
Roy Segall <roy.segall@gizra.com>
2016-05-13 01:34:29 +0300

    Let's start and test this bad ass.

---
+++ server/testing/README.md
@@ -0,0 +1,5 @@
+#  Library structure
+
+The library structured in the way that the **test** will contain unit testing
+oriented tests while the **BDD** library will contain test for the rest server
+and front end library.
---
+++ server/testing/test/plugins.js
@@ -0,0 +1,7 @@
+var assert = require('chai').assert;
+
+describe('Testing the plugin frameworks', function () {
+  it('should return -1 when the value is not present', function () {
+    assert.equal(-1, 'a');
+  });
+});
