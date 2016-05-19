435db986cfab10307a1a6a781b1fd3af496ca473
RoySegall <roy.segall@gizra.com>
2016-05-20 00:09:04 +0300

    Create new plugins.

--- server/modules/modules/users.js
+++ server/modules/modules/users.js
@@ -3,7 +3,21 @@ module.exports = {
   /**
    * Get all the users in the system.
    */
-  'getUsers': function() {
+  getUsers: function() {
+  },
+
+  /**
+   * Get a specific users.
+   */
+  getUser: function() {
+
+  },
+
+  /**
+   * Clear user cache.
+   */
+  clearUsers: function() {
+    this.users = [];
   }
 
 };
