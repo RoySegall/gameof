36879991124e53a6e486af985a80008d30afe42b
RoySegall <roy.segall@gizra.com>
2016-05-24 17:21:14 +0300

    If we have an access token don't create it again.

--- server/modules/plugins/express/access_token.js
+++ server/modules/plugins/express/access_token.js
@@ -17,41 +17,47 @@ function plugin() {
         new Error();
       }
 
-      gameOf.db.invokeCallback(gameOf.db.filter.bind(null, 'users', {
+      var filter = {
         'name': req.body.username,
         'pass': gameOf.users.encryptPassword(req.body.password)
-      }, this.generateAcessToken));
-    },
-
-    generateAcessToken: function(err, cursor) {
-      if (err) {
-        throw err;
-      }
-
-      cursor.toArray(function(err, result) {
+      };
+      gameOf.db.invokeCallback(gameOf.db.filter.bind(null, 'users', filter, function(err, cursor) {
         if (err) {
           throw err;
         }
 
-        if (result.length == 0) {
-          res.status(401);
-          gameOf.plug.jsonizer(res, {
-            'message': 'There is no user with the provided credentials.',
-            'statusCode': 401
-          });
-        }
-        else {
-          var user = result[0];
-          // Generate and access token and bind it to the user.
-          // todo: prevent generation in case we have one.
-          var access_token = gameOf.token.tokenGenerate(user);
-          gameOf.db.invokeCallback(gameOf.db.insertAsync.bind(null, 'access_token', access_token, function(err, info, bar) {
-            res.send(access_token);
-          }));
-        }
-      });
-    }
+        cursor.toArray(function(err, result) {
+          if (err) {
+            throw err;
+          }
 
+          if (result.length == 0) {
+            res.status(401);
+            gameOf.plug.jsonizer(res, {
+              'message': 'There is no user with the provided credentials.',
+              'statusCode': 401
+            });
+          }
+          else {
+            var user = result[0];
+            // Generate and access token and bind it to the user.
+            gameOf.db.invokeCallback(gameOf.db.filter.bind(null, 'access_token', {'uid': user.id}, function(err, cursor) {
+              cursor.toArray(function(err, result) {
+                if (result.length == 0) {
+                  var access_token = gameOf.token.tokenGenerate(user);
+                  gameOf.db.invokeCallback(gameOf.db.insertAsync.bind(null, 'access_token', access_token, function(err, info, bar) {
+                    res.send(access_token);
+                  }));
+                }
+                else {
+                  res.send(result[0]);
+                }
+              })
+            }));
+          }
+        });
+      }));
+    }
   };
 }
 
