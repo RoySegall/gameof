c4ddc8dec39098571941b5e11ad3fa83f7d2c9f8
RoySegall <roy.segall@gizra.com>
2016-05-27 14:09:28 +0300

    More refactor.

--- server/testing/bdd/access_token.js
+++ server/testing/bdd/access_token.js
@@ -3,58 +3,49 @@
  */
 var vows = require('vows'),
   assert = require('assert'),
-  request = require('request');
+  request = require('request'),
+  accessToken = require('../modules/access_token');
 
-var accessToken = {
+vows.describe('Testing user authentication')
+  .addBatch({
 
-  setAccessToken: function(access_token) {
-    this.accesstoken = access_token;
-  },
-
-  getAccessToken: function() {
-    return this.accesstoken;
-  }
-  
-};
-
-vows.describe('Testing user authentication').addBatch({
-
-  'Testing user validation': {
-    topic: function () {
-      request ({
-        uri: 'http://localhost:3000/api/access_token',
-        method: 'post',
-        form: {
-          username: 'roy segall',
-          password: 'roy'
-        }
-      }, this.callback );
-    },
-    'The server return 401 when passing wrong username and password': function (topic) {
-      assert.equal(401, topic.statusCode);
-    }
-  },
-
-  'Testing access token requiring': {
-    topic: function () {
-      request ({
-        uri: 'http://localhost:3000/api/access_token',
-        method: 'post',
-        form: {
-          username: 'roy segall',
-          password: 'roy segall'
-        }
-      }, this.callback );
+    'Testing user validation': {
+      topic: function () {
+        request ({
+          uri: 'http://localhost:3000/api/access_token',
+          method: 'post',
+          form: {
+            username: 'roy segall',
+            password: 'roy'
+          }
+        }, this.callback );
+      },
+      'The server return 401 when passing wrong username and password': function (topic) {
+        assert.equal(401, topic.statusCode);
+      }
     },
 
-    'The server return an access callback': function (topic) {
-      var body = JSON.parse(topic.toJSON().body);
-      assert.isNotEmpty(body.access_token);
-      accessToken.setAccessToken(body.access_token);
+    'Testing access token requiring': {
+      topic: function () {
+        request ({
+          uri: 'http://localhost:3000/api/access_token',
+          method: 'post',
+          form: {
+            username: 'roy segall',
+            password: 'roy segall'
+          }
+        }, this.callback );
+      },
+
+      'The server return an access callback': function (topic) {
+        var body = JSON.parse(topic.toJSON().body);
+        assert.isNotEmpty(body.access_token);
+        accessToken.setAccessToken(body.access_token);
+      }
     }
-  }
 })
 .addBatch({
+
   'Validating the access token': {
     topic: function () {
       request ({
@@ -73,4 +64,4 @@ vows.describe('Testing user authentication').addBatch({
     }
   }
 })
-  .export(module);
+.export(module);
