4cfffe513589353eba82a6f3466c6a5fb4a20d41
RoySegall <roy.segall@gizra.com>
2016-05-28 12:16:23 +0300

    Adding game display

--- server/modules/plugins/express/games.js
+++ server/modules/plugins/express/games.js
@@ -2,6 +2,8 @@
  * @id games
  * @path /api/games
  *
+ * @subRoute /:id, /:id/:name
+ *
  * @get restGet
  * @post postGet
  */
@@ -10,7 +12,24 @@ function plugin() {
   return {
 
     restGet: function(req, res) {
-      module.exports.gameOf.plug.jsonizer(res, {'foo': 'bar', 'bar': 'foo'});
+      var db = module.exports.gameOf.db;
+      var plug = module.exports.gameOf.plug;
+
+      var filter = {};
+
+      if (req.params.id != undefined) {
+        filter.id = req.params.id;
+      }
+
+      db.invokeCallback(db.filter.bind(null, 'games', filter, function(err, cursor) {
+        if (err) {
+          throw err;
+        }
+
+        cursor.toArray(function(err, result) {
+          plug.jsonizer(res, result)
+        });
+      }));
     },
 
     postGet: function(req, res) {
